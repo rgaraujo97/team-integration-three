c7ac0e5c8d8cff4972f14e117c9ea8480137a016
Yolanda Vicente Sampaio <yvicente@minsait.com>
2021-11-30 09:58:59 +0000

    login inputs

---
+++ src/views/App/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <p>Yolanda Vicente Sampaio - Team Integration Three</p>
+        </footer>
+    )
+}
+
+export default Footer
--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Login = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    
+  }
+
   return (
-   <div></div>
+    <div>
+      <form type='submit'>
+        <input type='text' name='username' value={username} onChange={(event) => setUsername(event.target.value)} />
+        <input type='password' name='password' value={password} onChange={(event) => setPassword(event.target.value)} />
+        <button onClick={handleSubmit}>Login</button>
+      </form>
+   </div>
   );
 }
 
